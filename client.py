26ea1840f56a7c74a35f91d339bd9ed5a7e7b04a
Anish Pradhan <anishpradhan@Anishs-MacBook-Pro.local>
2019-04-27 17:48:47 -0700

    mongo db

--- client.py
+++ client.py
@@ -9,6 +9,8 @@ from src.constants import SKIPTRIP_HOST, SKIPTRIP_URL
 import csv
 from pymongo import MongoClient, errors
 
+price_date = datetime.datetime.today().strftime('%Y-%m-%d')
+
 
 def api_query(SKIPTRIP_HOST, params):
     try:
@@ -63,11 +65,13 @@ def filter_by_price(flights, price_limit):
                 mongo_data["data_key"] = data[0]
                 main_data = json.loads(data[1])
                 mongo_data["flight"] = main_data.get("legs", [])
+                mongo_data["departure"] = mongo_data["flight"][0][2]
+                mongo_data["origin"] = mongo_data["flight"][0][1]
+                mongo_data["destination"] = mongo_data["flight"][0][3]
                 mongo_data["stops"] = v.get("count", 0)
                 mongo_data["segments"] = v.get("segments", [])
                 mongo_data["key"] = main_data.get("key","")
-                mongo_data["duration"] = v.get("duration", 0)
-                price_date = datetime.datetime.today().strftime('%Y-%m-%d')
+                mongo_data["duration"] = v.get("duration", 0)/3600.0
                 mongo_data[price_date] = price
                 data.append(price)
                 flight_data.append(mongo_data)
@@ -83,9 +87,22 @@ def mongo_client():
     except errors.ConnectionFailure as e:
         logging.info("Connection Failure: {}".format(e))
     else:
-        db = client.flights_db_t
-        col = db.flights_col_t
+        db = client.flights_db
+        col = db.flights_col
         return col
+
+
+def mongo_insert(col, final_data):
+    for data in final_data:
+        # print data
+        mongo_record = col.find_one({"key": data.get("key", 0)})
+        if mongo_record:
+            logging.info("Updating Data with key: {}".format(data["key"]))
+            col.update({"_id": mongo_record["_id"]}, {"$set": {price_date: data[price_date]}})
+        else:
+            logging.info("Inserting Data with key: {}".format(data["key"]))
+            post_id = col.insert_one(data).inserted_id
+            logging.info("Inserted Data with post_id: {}".format(post_id))
 
 
 def write_csv(input, filename):
@@ -98,24 +115,32 @@ def write_csv(input, filename):
 
 if __name__ == '__main__':
     import logging; logging.basicConfig(level=logging.DEBUG)
-    price_limit = 750
-    flights = flights_by_day(origin="SFO", dest="PHL", depart="2019-04-28", ret="", flight_time="evening", weeks=0,
-                             stop=2)
-    final_data = filter_by_price(flights, price_limit)
-    for data in final_data:
-        print data
-    sys.exit(0)
+    price_limit = 250
+    flights_sfo = flights_by_day(origin="SFO", dest="PHL", depart="2019-06-14", ret="", flight_time="evening", weeks=0,
+                                 stop=2)
+    flights_oak = flights_by_day(origin="OAK", dest="PHL", depart="2019-06-14", ret="", flight_time="evening", weeks=0,
+                                 stop=2)
+    flights_phl_mor = flights_by_day(origin="PHL", dest="SFO", depart="2019-06-17", ret="", flight_time="morning",
+                                     weeks=0, stop=2)
+    flights_phl_eve = flights_by_day(origin="PHL", dest="SFO", depart="2019-06-17", ret="", flight_time="evening",
+                                     weeks=0, stop=2)
+    flights = flights_sfo + flights_oak + flights_phl_eve + flights_phl_mor
+    full_data = filter_by_price(flights, price_limit)
     col = mongo_client()
-    col.insert()
-    print final_data
-    # write_csv(final_data, "sfo_phl")
-    sys.exit(0)
-    flights = flights_by_day(origin="OAK", dest="PHL", depart="2019-04-12", ret="", flight_time="evening", weeks=7,
-                             stop=2)
-    final_data = filter_by_price(flights, price_limit)
-    write_csv(final_data, "oak_phl")
-
-    flights = flights_by_day(origin="PHL", dest="SFO", depart="2019-04-15", ret="", flight_time="morning", weeks=7,
-                             stop=2)
-    final_data = filter_by_price(flights, price_limit)
-    write_csv(final_data, "phl_sfo")
+    mongo_insert(col, full_data)
+    flights_sfo = flights_by_day(origin="SFO", dest="PHL", depart="2019-07-03", ret="", flight_time="evening", weeks=0,
+                                 stop=2)
+    flights_oak = flights_by_day(origin="OAK", dest="PHL", depart="2019-07-03", ret="", flight_time="evening", weeks=0,
+                                 stop=2)
+    flights_phl_mor = flights_by_day(origin="PHL", dest="SFO", depart="2019-07-07", ret="", flight_time="morning",
+                                     weeks=0, stop=2)
+    flights_phl_eve = flights_by_day(origin="PHL", dest="SFO", depart="2019-07-07", ret="", flight_time="evening",
+                                     weeks=0, stop=2)
+    flights_phl_mor_1 = flights_by_day(origin="PHL", dest="SFO", depart="2019-07-08", ret="", flight_time="morning",
+                                       weeks=0, stop=2)
+    flights_phl_eve_1 = flights_by_day(origin="PHL", dest="SFO", depart="2019-07-08", ret="", flight_time="evening",
+                                       weeks=0, stop=2)
+    flights = flights_sfo + flights_oak + flights_phl_eve + flights_phl_mor + flights_phl_eve_1 + flights_phl_mor_1
+    full_data = filter_by_price(flights, price_limit)
+    col = mongo_client()
+    mongo_insert(col, full_data)
